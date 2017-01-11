db5730374accf850eeb2a1ea220180285ae37680
djrosl <dubztep45@gmail.com>
2017-01-11 15:12:58 +0200

    fixes

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -20,6 +20,7 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
+    <link rel="icon" href="/favicon.png">
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
