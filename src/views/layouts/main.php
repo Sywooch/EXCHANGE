e9f31ca2520e8f1eaf38e50f1e61d0021954a1b6
djrosl <dubztep45@gmail.com>
2017-01-14 21:43:38 +0200

    fixes

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -209,7 +209,7 @@ $icq = Credential::findOne(['slug'=>'icq'])->value;
 
 	<?php ActiveForm::end(); ?>
 </div>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5533fc4d5e0d525f"></script>
+<!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-587a7e67ade82ba8"></script>
 <?php $this->endBody() ?>
 </body>
 </html>
