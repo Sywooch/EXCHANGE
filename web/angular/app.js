034ae13cba53c4abac2b608f826754625d2471e4
djrosl <dubztep45@gmail.com>
2017-01-23 20:45:38 +0200

    fixes

--- web/angular/app.js
+++ web/angular/app.js
@@ -96,7 +96,7 @@ app.controller('FormController', ['$scope', '$http', '$timeout', function($scope
     };
 
     $scope.countExchangeResult = function(){
-        var result = $scope.exchange_from ? (parseFloat($scope.exchange_from) * parseFloat($scope.directionActive.courseCounted)).toFixed(6) : 0;
+        var result = $scope.exchange_from ? (parseFloat($scope.exchange_from) * parseFloat($scope.directionActive.courseCounted)).toFixed(2) : 0;
 
         if(($scope.exchange_from * parseFloat($scope.directionActive.exchange_percent) / 100) < $scope.directionActive.min_comission) {
             return 0;
